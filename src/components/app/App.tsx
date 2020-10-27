1a02a5275a7b517b3d030992159f935ad96e9ac2
Karen Garcia <kgarcia@i3screen.com>
2020-10-27 16:35:01 -0600

    Initial commit of new app

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from '../../assests/sayKaren_logo.png';
+import '../../styling/App.scss';
 
 function App() {
   return (
