1d430532ee4c2a639a48c1b3085789c8771b2546
Karen Garcia <saykaren@gmail.com>
2021-02-12 15:24:35 -0700

    Adding CheckBoxes
    Added checkboxes to grocery section and headers

--- src/components/app/Groceries.tsx
+++ src/components/app/Groceries.tsx
@@ -12,127 +12,172 @@ interface GroceriesProp {
 const Groceries = ({}: GroceriesProp) => {
   const [data, setData] = useState(RecipeData);
 
-
   return (
     <div className="GroceriesSection">
       <h2>Groceries</h2>
-    <section className="ContainerGroceryTable">
-      {data.map((dataItem, index) => (
-        <div key={index}>
-          {dataItem.groceryList && (
-            <div >
-              {dataItem.ingredients
-                .sort((a, b) => a.type.localeCompare(b.type))
-                .filter((item) => item.section === "Produce")
-                .map((ingred, ingredIndex) => (
-                  <span key={ingredIndex} className="rowGrocery grocery ">
-                    {ingred.type} {ingred.amount}
-                  </span>
-                ))}
-            </div>
-          )}
-        </div>
-      ))}
-    {data.map((dataItem, index) => (
-        <div key={index}>
-          {dataItem.groceryList && (
-            <div >
-              {dataItem.ingredients
-                .sort((a, b) => a.type.localeCompare(b.type))
-                .filter((item) => item.section === "Bread")
-                .map((ingred, ingredIndex) => (
-                  <span key={ingredIndex} className="rowGrocery grocery ">
-                    {ingred.type} {ingred.amount}
-                  </span>
-                ))}
-            </div>
-          )}
-        </div>
-      ))}
-      {data.map((dataItem, index) => (
-        <div key={index}>
-          {dataItem.groceryList && (
-            <div >
-              {dataItem.ingredients
-                .sort((a, b) => a.type.localeCompare(b.type))
-                .filter((item) => item.section === "NonPerishables")
-                .map((ingred, ingredIndex) => (
-                  <span key={ingredIndex} className="rowGrocery grocery ">
-                    {ingred.type} {ingred.amount}
-                  </span>
-                ))}
-            </div>
-          )}
-        </div>
-      ))}
+      <section className="ContainerGroceryTable">
+        <span className="rowGrocery grocery titleGrocery">Produce</span>
+        {data.map((dataItem, index) => (
+          <div key={index}>
+            {dataItem.groceryList && (
+              <div>
+                {dataItem.ingredients
+                  .sort((a, b) => a.type.localeCompare(b.type))
+                  .filter((item) => item.section === "Produce")
+                  .map((ingred, ingredIndex) => (
+                    <span key={ingredIndex} className="rowGrocery grocery ">
+                      <input
+                        type="checkbox"
+                        id={ingred.type}
+                        name={ingred.type}
+                        value={ingred.type}
+                      ></input>
+                      {ingred.type} {ingred.amount}
+                    </span>
+                  ))}
+              </div>
+            )}
+          </div>
+        ))}
+        <span className="rowGrocery grocery titleGrocery">Bread</span>
+        {data.map((dataItem, index) => (
+          <div key={index}>
+            {dataItem.groceryList && (
+              <div>
+                {dataItem.ingredients
+                  .sort((a, b) => a.type.localeCompare(b.type))
+                  .filter((item) => item.section === "Bread")
+                  .map((ingred, ingredIndex) => (
+                    <span key={ingredIndex} className="rowGrocery grocery ">
+                      <input
+                        type="checkbox"
+                        id={ingred.type}
+                        name={ingred.type}
+                        value={ingred.type}
+                      ></input>
+                      {ingred.type} {ingred.amount}
+                    </span>
+                  ))}
+              </div>
+            )}
+          </div>
+        ))}
+        <span className="rowGrocery grocery titleGrocery">Non-Perishables</span>
+        {data.map((dataItem, index) => (
+          <div key={index}>
+            {dataItem.groceryList && (
+              <div>
+                {dataItem.ingredients
+                  .sort((a, b) => a.type.localeCompare(b.type))
+                  .filter((item) => item.section === "NonPerishables")
+                  .map((ingred, ingredIndex) => (
+                    <span key={ingredIndex} className="rowGrocery grocery ">
+                      <input
+                        type="checkbox"
+                        id={ingred.type}
+                        name={ingred.type}
+                        value={ingred.type}
+                      ></input>{" "}
+                      {ingred.type} {ingred.amount}
+                    </span>
+                  ))}
+              </div>
+            )}
+          </div>
+        ))}
+        <span className="rowGrocery grocery titleGrocery">Meat</span>
+        {data.map((dataItem, index) => (
+          <div key={index}>
+            {dataItem.groceryList && (
+              <div>
+                {dataItem.ingredients
+                  .sort((a, b) => a.type.localeCompare(b.type))
+                  .filter((item) => item.section === "Meat")
+                  .map((ingred, ingredIndex) => (
+                    <span key={ingredIndex} className="rowGrocery grocery ">
+                      <input
+                        type="checkbox"
+                        id={ingred.type}
+                        name={ingred.type}
+                        value={ingred.type}
+                      ></input>
+                      {ingred.type} {ingred.amount}
+                    </span>
+                  ))}
+              </div>
+            )}
+          </div>
+        ))}
+        <span className="rowGrocery grocery titleGrocery">Personal Care</span>
+        {data.map((dataItem, index) => (
+          <div key={index}>
+            {dataItem.groceryList && (
+              <div>
+                {dataItem.ingredients
+                  .sort((a, b) => a.type.localeCompare(b.type))
+                  .filter((item) => item.section === "Personal Care")
+                  .map((ingred, ingredIndex) => (
+                    <span key={ingredIndex} className="rowGrocery grocery ">
+                      <input
+                        type="checkbox"
+                        id={ingred.type}
+                        name={ingred.type}
+                        value={ingred.type}
+                      ></input>
+                      {ingred.type} {ingred.amount}
+                    </span>
+                  ))}
+              </div>
+            )}
+          </div>
+        ))}
+        <span className="rowGrocery grocery titleGrocery">Dairy</span>
+        {data.map((dataItem, index) => (
+          <div key={index}>
+            {dataItem.groceryList && (
+              <div>
+                {dataItem.ingredients
+                  .sort((a, b) => a.type.localeCompare(b.type))
+                  .filter((item) => item.section === "Dairy")
+                  .map((ingred, ingredIndex) => (
+                    <span key={ingredIndex} className="rowGrocery grocery ">
+                      <input
+                        type="checkbox"
+                        id={ingred.type}
+                        name={ingred.type}
+                        value={ingred.type}
+                      ></input>{" "}
+                      {ingred.type} {ingred.amount}
+                    </span>
+                  ))}
+              </div>
+            )}
+          </div>
+        ))}
 
-      {data.map((dataItem, index) => (
-        <div key={index}>
-          {dataItem.groceryList && (
-            <div >
-              {dataItem.ingredients
-                .sort((a, b) => a.type.localeCompare(b.type))
-                .filter((item) => item.section === "Meat")
-                .map((ingred, ingredIndex) => (
-                  <span key={ingredIndex} className="rowGrocery grocery ">
-                    {ingred.type} {ingred.amount}
-                  </span>
-                ))}
-            </div>
-          )}
-        </div>
-      ))}
-
-    {data.map((dataItem, index) => (
-        <div key={index}>
-          {dataItem.groceryList && (
-            <div >
-              {dataItem.ingredients
-                .sort((a, b) => a.type.localeCompare(b.type))
-                .filter((item) => item.section === "Personal Care")
-                .map((ingred, ingredIndex) => (
-                  <span key={ingredIndex} className="rowGrocery grocery ">
-                    {ingred.type} {ingred.amount}
-                  </span>
-                ))}
-            </div>
-          )}
-        </div>
-      ))}
-  {data.map((dataItem, index) => (
-        <div key={index}>
-          {dataItem.groceryList && (
-            <div >
-              {dataItem.ingredients
-                .sort((a, b) => a.type.localeCompare(b.type))
-                .filter((item) => item.section === "Dairy")
-                .map((ingred, ingredIndex) => (
-                  <span key={ingredIndex} className="rowGrocery grocery ">
-                    {ingred.type} {ingred.amount}
-                  </span>
-                ))}
-            </div>
-          )}
-        </div>
-      ))}
-
-{data.map((dataItem, index) => (
-        <div key={index}>
-          {dataItem.groceryList && (
-            <div >
-              {dataItem.ingredients
-                .sort((a, b) => a.type.localeCompare(b.type))
-                .filter((item) => item.section === "Frozen")
-                .map((ingred, ingredIndex) => (
-                  <span key={ingredIndex} className="rowGrocery grocery ">
-                    {ingred.type} {ingred.amount}
-                  </span>
-                ))}
-            </div>
-          )}
-        </div>
-      ))}
-</section>
+        {data.map((dataItem, index) => (
+          <div key={index}>
+            {dataItem.groceryList && (
+              <div>
+                {dataItem.ingredients
+                  .sort((a, b) => a.type.localeCompare(b.type))
+                  .filter((item) => item.section === "Frozen")
+                  .map((ingred, ingredIndex) => (
+                    <span key={ingredIndex} className="rowGrocery grocery ">
+                      <input
+                        type="checkbox"
+                        id={ingred.type}
+                        name={ingred.type}
+                        value={ingred.type}
+                      ></input>
+                      {ingred.type} {ingred.amount}
+                    </span>
+                  ))}
+              </div>
+            )}
+          </div>
+        ))}
+      </section>
     </div>
   );
 };
