02fbaa6cc398ecf2af88c370a31b16222603fe87
Karen Garcia <saykaren@gmail.com>
2021-08-20 15:57:10 -0600

    Maps through results and adds picture and youtube video

---
+++ src/components/app/MealDetails.tsx
@@ -0,0 +1,41 @@
+import * as React from "react";
+
+export interface MealDetailsProps {
+  meal: any;
+}
+
+const MealDetails = ({ meal }: MealDetailsProps) => {
+  return (
+    <div className="mealDetails">
+      <div className="mealHeader">
+        {meal.strMealThumb && (
+          <img
+            src={meal.strMealThumb}
+            alt="Meal picture"
+            className="mealPicture"
+          />
+        )}
+        {meal.strMeal && <h3>Name: {meal.strMeal}</h3>}
+      </div>
+
+      {meal.strCategory && <p>Category: {meal.strCategory}</p>}
+      {meal.strInstructions && (
+        <details>
+          <summary>Instructions:</summary>
+          {meal.strInstructions}
+        </details>
+      )}
+      {meal.strYoutube && (
+        <p>
+          <a href={meal.strYoutube} target="_blank" rel="noopener noreferrer">
+            {" "}
+            YouTube Video
+          </a>
+        </p>
+      )}
+
+    </div>
+  );
+};
+
+export default MealDetails;
