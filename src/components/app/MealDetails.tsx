8fcdc9353d8bbe42f6127dbb634104ecc7db78b2
Karen Garcia <saykaren@gmail.com>
2021-08-24 15:28:03 -0600

    Adds ingredients

--- src/components/app/MealDetails.tsx
+++ src/components/app/MealDetails.tsx
@@ -4,6 +4,10 @@ export interface MealDetailsProps {
   meal: any;
 }
 
+const ingredientArray = [
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+];
+
 const MealDetails = ({ meal }: MealDetailsProps) => {
   return (
     <div className="mealDetails">
@@ -24,6 +28,21 @@ const MealDetails = ({ meal }: MealDetailsProps) => {
           {meal.strInstructions}
         </details>
       )}
+      <details>
+        <summary>Ingredients</summary>
+        {ingredientArray.map((numberArray, indexArray) => (
+          <div key={indexArray}>
+            {" "}
+            {meal[`strIngredient${numberArray}`] &&
+              meal[`strIngredient${numberArray}`].length > 1 && (
+                <li>
+                  {meal[`strIngredient${numberArray}`]}:{" "}
+                  {meal[`strMeasure${numberArray}`]}
+                </li>
+              )}
+          </div>
+        ))}
+      </details>
       {meal.strYoutube && (
         <p>
           <a href={meal.strYoutube} target="_blank" rel="noopener noreferrer">
