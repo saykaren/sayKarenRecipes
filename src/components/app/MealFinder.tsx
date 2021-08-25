af67788b2635d6d3158233652dacc4de977cae4f
Karen Garcia <saykaren@gmail.com>
2021-08-25 16:06:47 -0600

    Adds random feature to meal searcher

--- src/components/app/MealFinder.tsx
+++ src/components/app/MealFinder.tsx
@@ -1,3 +1,4 @@
+import { cleanup } from "@testing-library/react";
 import React, { useState } from "react";
 import { useQuery } from "react-query";
 import mealFinderAPI from "../Fetch/MealFinderAPI";
@@ -6,29 +7,38 @@ import MealDetails from "./MealDetails";
 export interface MealFinderProps {}
 
 const MealFinder = ({}: MealFinderProps) => {
-
   const [mealName, setMealName] = useState("Arrabiat");
   const [searchTerm, setSearchTerm] = useState("");
-  const {data, status} = useQuery(
+  const [apiSearchType, setAPISearchType] = useState("search.php?s=");
+  const { data, status } = useQuery(
     [
       "meal",
       "https://www.themealdb.com/api/json/v1/1/",
-      "search.php?s=",
+      `${apiSearchType}`,
       `${mealName}`,
     ],
     mealFinderAPI
   );
 
-  const updateSearch = (valueMeal: string) => {
+  const apiTypicalSearch = "search.php?s=";
+  const apiRandomSearch = "random.php";
+
+  const updateSearch = (valueMeal: string, searchType: string) => {
     const mealNameUpdate = valueMeal.replaceAll(" ", "_");
     setMealName(mealNameUpdate);
+    setAPISearchType(searchType);
+  };
+
+  const cleanUp = () => {
+    setSearchTerm("Arrabiat");
+    setAPISearchType(apiTypicalSearch);
   };
 
   return (
     <div id="MealFinder">
-     <h3 id="MealTitle"> Meal Searcher</h3>
+      <h3 id="MealTitle"> Meal Searcher</h3>
       <div className="MealHeader">
-           <input
+        <input
           type="text"
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
@@ -36,28 +46,46 @@ const MealFinder = ({}: MealFinderProps) => {
           id="inputSearchTerm"
           className="filterBuddydetail"
         />
-        <div className="button" onClick={() => updateSearch(searchTerm)}>
-          Search
-        </div>
+        <section className="buttonSection">
+          <div
+            className="button"
+            onClick={() => updateSearch(searchTerm, apiTypicalSearch)}
+          >
+            Search
+          </div>
+          <div
+            className="button"
+            onClick={() => updateSearch("", apiRandomSearch)}
+          >
+            Random Recipe!
+          </div>
+        </section>
       </div>
       {status === "error" && (
-    <div>
-      Error Fetching Data <button onClick={() => console.log('clean up')}>Clear</button>
-    </div>
-  )}
-  {status === "loading" && <div className="cssAcquaintances">Loading Data....</div>}
-  {status === "success" && ( 
-      <div className="mealFinderContainer ">
-        {data.meals && data.meals.length >= 1 &&
-          data.meals.map((meal: any, mealIndex: number) => (
-            <div key={mealIndex} className="cssAcquaintances">
-              <MealDetails meal={meal} />
+        <div>
+          Error Fetching Data{" "}
+          <button className="button" onClick={() => cleanUp()}>
+            Clear
+          </button>
+        </div>
+      )}
+      {status === "loading" && (
+        <div className="cssAcquaintances">Loading Data....</div>
+      )}
+      {status === "success" && (
+        <div className="mealFinderContainer ">
+          {data.meals &&
+            data.meals.length >= 1 &&
+            data.meals.map((meal: any, mealIndex: number) => (
+              <div key={mealIndex} className="cssAcquaintances">
+                <MealDetails meal={meal} />
+              </div>
+            ))}
+          {data.meals == null && (
+            <div className="cssAcquaintances boldFont">
+              Sorry no meals with {mealName} found!
             </div>
-          ))} 
-                 { data.meals == null && (
-         <div className="cssAcquaintances boldFont">Sorry no meals with {mealName} found!
-            </div>
-          )} 
+          )}
         </div>
       )}
     </div>
