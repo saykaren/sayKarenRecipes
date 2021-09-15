1c26639307e181ba9d66a18161d806ec6fd8a774
Karen Garcia <saykaren@gmail.com>
2021-09-15 15:41:53 -0600

    98 Removes unused useRef

--- src/components/app/RecipeSection.tsx
+++ src/components/app/RecipeSection.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import RecipeData from "../data/data";
 import RecipeDataInterface from "../Interface/RecipeDataInterface";
 import Lilly_Good from "../assests/Lilly_Good.jpg";
